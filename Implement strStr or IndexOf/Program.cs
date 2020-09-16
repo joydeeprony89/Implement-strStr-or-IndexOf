316c75836a8aa2404f54bf182347c17f730cab27
Jaydeep Shil <ED8A@eurofins.com>
2020-09-16 13:09:01 +0530

    Add project files.

---
+++ Implement strStr or IndexOf/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Implement_strStr_or_IndexOf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the haystack !");
+            var haystack = Console.ReadLine();
+            Console.WriteLine("Enter the needle !");
+            var needle = Console.ReadLine();
+            Console.WriteLine(StrStr(haystack, needle));
+        }
+
+        static int StrStr(string haystack, string needle)
+        {
+            if (string.IsNullOrWhiteSpace(needle)) return 0;
+            int haystackLength = haystack.Length;
+            int needleLength = needle.Length;
+            if (haystack == needle) return 0;
+            if(needleLength > haystackLength) return -1;
+            for(int i =0; haystackLength - i >= needleLength; i++)
+            {
+                if (haystack.Substring(i, needleLength) == needle) return i;
+            }
+
+            return -1;
+        }
+    }
+}
